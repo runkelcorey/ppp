8952fac876b2bfcd384472f4f14d20daa3365a43
Corey Runkel <runkelcorey@gmail.com>
2020-07-08 11:09:34 -0400

    Created density plot

--- visualize.R
+++ visualize.R
@@ -7,7 +7,6 @@
 #libraries
 library(tidyverse)
 library(lubridate)
-library(sf)
 
 #pre-processing: requires all_loans dataset created from import.R script
 all_loans <- mutate(all_loans, LoanRange = ifelse(is.na(LoanRange), "", as.character(factor(LoanRange, levels = c("e $150,000-350,000", "d $350,000-1 million", "c $1-2 million", "b $2-5 million", "a $5-10 million"), labels = c("$150k-$350k", "$350k-$1m", "$1m-$2m", "$2m-$5m", "$5m-$10m")))), #create factor from character
@@ -35,3 +34,12 @@ all_loans %>%
   scale_x_date(date_breaks = "2 weeks", date_labels = "%B %d", expand = expansion(mult = .01)) + #increase frequency of x axis ticks (1 week = 1 year in COVID)
   scale_y_continuous(labels = scales::percent) + #label axes by percent
   labs(x = "Approval date", y = "Percentage of daily loans", title = "Daily distribution of 4,931,453 PPP loan approvals", subtitle = "April 3, 2020 - June 30, 2020", caption = "Source: Small Business Association", fill = "Loan range")
+
+#size distribution of loan approvals below $150k
+all_loans %>%
+  filter(LoanAmount > 0) %>% #remove NAs and that one loan that was -$2 million
+  ggplot() +
+  geom_density(aes(LoanAmount), color = "#286dc0") + #density along loan amount
+  scale_x_continuous(labels = scales::comma) + #prevent scientific notation
+  scale_y_continuous(labels = scales::percent) +
+  labs(x = "Size of loan approved ($)", y = "Frequency", title = "Distribution of PPP loan approvals below $150,000", subtitle = "Data covers 4,270,162 loans approved between April 3 and June 30, 2020", caption = "Source: Small Business Administration")
